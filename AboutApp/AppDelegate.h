2c54d70cc8f2ce1cc1017ab55bfece4c62582cea
khushboo <khushboo@khushboos-mac-mini.local>
2016-07-18 20:01:15 +0530

    Initial Commit

---
+++ AboutApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AboutApp
+//
+//  Created by khushboo on 18/07/16.
+//  Copyright © 2016 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
